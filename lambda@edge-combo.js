8fc2d528f331b50c371a34e1fe17343a9987e562
Viljami Kuosmanen <viljami@avoinsorsa.fi>
2020-03-12 22:25:59 +0100

    Add debug

--- lambda@edge-combo.js
+++ lambda@edge-combo.js
@@ -1,4 +1,4 @@
-const { S3 } = require('aws-sdk');
+iconst { S3 } = require('aws-sdk');
 const crypto = require('crypto')
 
 exports.handler = async (event, context) => {
@@ -6,31 +6,47 @@ exports.handler = async (event, context) => {
     const { uri, querystring } = event.Records[0].cf.request;
     console.log(uri, querystring);
 
-    const parts = querystring.split('.');
     const hash = crypto.createHash('md5').update(`combo?${querystring}`).digest('hex');
 
     const Key = `combo/${hash}`;
     console.log(Key);
 
-    const s3 = new S3();
-    const obj = await s3.getObject({
-        Bucket: 'thl-fi-mirror',
-        Key,
-    }).promise();
-    console.log(obj);
+    let response;
 
-    const response = {
-        status: '200',
-        statusDescription: 'OK',
-        headers: {
-            'content-type': [{
-                key: 'Content-Type',
-                value: obj.ContentType,
-            }]
-        },
-        body: obj.Body.toString('utf-8'),
-    };
+    try {
+        const s3 = new S3();
+        const obj = await s3.getObject({
+            Bucket: 'thl-fi-mirror',
+            Key,
+        }).promise();
+        console.log(obj);
 
+        response = {
+            status: '200',
+            statusDescription: 'OK',
+            headers: {
+                'content-type': [{
+                    key: 'Content-Type',
+                    value: obj.ContentType,
+                }]
+            },
+            body: obj.Body.toString('utf-8'),
+        };
+    } catch (err) {
+        response = {
+            status: '200',
+            statusDescription: 'OK',
+            headers: {
+                'content-type': [{
+                    key: 'Content-Type',
+                    value: 'application/json',
+                }]
+            },
+            body: JSON.stringify({ err, Key, event })
+        };
+    }
+
+    console.log(response);
     return response;
 };
 
